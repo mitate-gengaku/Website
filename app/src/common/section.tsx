d4d99d9d820daa044593394a65c975f93265ad88
mitate-gengaku <mitategengaku@gmail.com>
2025-09-28 22:29:46 +0900

    add: Sectionコンポーネントの作成 #1

---
+++ app/src/common/section.tsx
@@ -0,0 +1,10 @@
+import type React from "react";
+
+import { cn } from "@/utils/cn"
+
+export const Section = ({
+  className,
+  ...props
+}: React.ComponentProps<"section">) => (
+  <section className={cn("px-4 py-24 lg:px-8", className)} {...props} />
+);
