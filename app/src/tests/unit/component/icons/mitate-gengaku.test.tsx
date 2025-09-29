484dedcb0291fdd8c7ebff26d8144d6960ba617b
mitate-gengaku <mitategengaku@gmail.com>
2025-09-29 18:26:47 +0900

    test: mitete-gangaku.test.tsxの修正 #1

--- app/src/tests/unit/component/icons/mitate-gengaku.test.tsx
+++ app/src/tests/unit/component/icons/mitate-gengaku.test.tsx
@@ -7,25 +7,23 @@ describe("MitateGengakuAscii コンポーネント", () => {
 
   afterEach(cleanup);
 
-  describe("モックデータを使用しない", () => {
-    beforeEach(() => {
-      rendered = render(<MitateGengakuAscii data-testid="mitate-gengaku" />);
-    });
+  beforeEach(() => {
+    rendered = render(<MitateGengakuAscii data-testid="mitate-gengaku" />);
+  });
 
-    test("コンポーネントが正しくレンダリングされること", () => {
-      expect(rendered.getByTestId("mitate-gengaku")).toBeInTheDocument();
-    });
+  test("コンポーネントが正しくレンダリングされること", () => {
+    expect(rendered.getByTestId("mitate-gengaku")).toBeInTheDocument();
+  });
 
-    test("コンポーネントのタグ名は'div'であること", () => {
-      expect(rendered.getByTestId("mitate-gengaku").tagName).toEqual("DIV");
-    });
+  test("コンポーネントのタグ名は'div'であること", () => {
+    expect(rendered.getByTestId("mitate-gengaku").tagName).toEqual("DIV");
+  });
 
-    test("コンポーネント内の'span'タグはclassNameを持っていること", () => {
-      const spans = rendered.container.querySelectorAll("span");
+  test("コンポーネント内の'span'タグはclassNameを持っていること", () => {
+    const spans = rendered.container.querySelectorAll("span");
 
-      for (let i = 0; i < spans.length; i++) {
-        expect(spans[i].className).toEqual("block h-4 w-2 [zoom:0.2]");
-      }
-    });
+    for (let i = 0; i < spans.length; i++) {
+      expect(spans[i].className).toEqual("block h-4 w-2 [zoom:0.2]");
+    }
   });
 });
