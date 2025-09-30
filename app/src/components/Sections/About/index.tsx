a96f3a3b11779eb0c9373a2299cb008b2d03418b
mitate-gengaku <mitategengaku@gmail.com>
2025-09-30 18:09:29 +0900

    test: index.test.tsxの作成 #5

--- app/src/components/Sections/About/index.tsx
+++ app/src/components/Sections/About/index.tsx
@@ -5,7 +5,7 @@ import { Description } from "@/components/Sections/About/description";
 import { ProfileAscii } from "@/components/Sections/About/profile-ascii";
 
 export const About = () => (
-  <Section id="about">
+  <Section id="about" data-testid="about">
     <div className="mx-auto flex flex-col lg:flex-row lg:items-center gap-6 max-w-5xl">
       <div className="space-y-6 text-center lg:text-left">
         <Title title="Hello. I am 【御楯玄嶽】" />
---
+++ app/src/tests/unit/component/Sections/About/index.test.tsx
@@ -0,0 +1,45 @@
+import { cleanup, render, type RenderResult } from "@testing-library/react";
+
+import type React from "react";
+
+import { About } from "@/components/Sections/About";
+
+vi.mock("@/components/Common/section", () => ({
+  Section: (props: React.ComponentProps<"section">) => (
+    <section data-testid="about" {...props} />
+  ),
+}));
+
+vi.mock("@/components/Common/title", () => ({
+  Title: () => <div data-testid="title" />,
+}));
+
+vi.mock("@/components/Common/description", () => ({
+  Description: () => <div data-testid="description" />,
+}));
+
+vi.mock("@/components/Common/buttons", () => ({
+  Buttons: () => <div data-testid="buttons" />,
+}));
+
+vi.mock("@/components/Sections/About/profile-ascii", () => ({
+  ProfileAscii: () => <div data-testid="profile-ascii" />,
+}));
+
+describe("Aboutコンポーネント", () => {
+  let rendered: RenderResult;
+
+  afterEach(cleanup);
+
+  beforeEach(() => {
+    rendered = render(<About />);
+  });
+
+  test("コンポーネントが正しくレンダリングされること", () => {
+    expect(rendered.getByTestId("about")).toBeInTheDocument();
+    expect(rendered.getByTestId("title")).toBeInTheDocument();
+    expect(rendered.getByTestId("description")).toBeInTheDocument();
+    expect(rendered.getByTestId("buttons")).toBeInTheDocument();
+    expect(rendered.getByTestId("profile-ascii")).toBeInTheDocument();
+  });
+});
