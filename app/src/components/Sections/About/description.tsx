09bba1bfea79059a0cbabf1aa1b5a1e5d4ba14e2
mitate-gengaku <mitategengaku@gmail.com>
2025-09-30 16:41:53 +0900

    add: Descriptionコンポーネントの作成 #5

---
+++ app/src/components/Sections/About/description.tsx
@@ -0,0 +1,9 @@
+export const Description = () => (
+  <div className="text-sm lg:text-base">
+    <p>I am a junior engineer in the computer infrastructure area.</p>
+    <p>
+      I became interested in programming in my late teens, and I'm currently
+      playing around with Linux.
+    </p>
+  </div>
+);
